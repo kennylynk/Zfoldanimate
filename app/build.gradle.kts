696a7ad0ef810ceecd86d935aa12e750464901ef
kennylynk <92709198+kennylynk@users.noreply.github.com>
2026-09-12 04:09:17 +0900

    Add lifecycle-runtime-ktx and core-animation-ktx dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,4 +35,6 @@ dependencies {
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("androidx.window:window:1.3.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.4")
+    implementation("androidx.core:core-animation-ktx:1.0.0")
 }
